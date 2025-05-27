8ef3afc070f99375950d3f16062d9a647cf10943
ziu03 <57125940+sonjiwoo031105@users.noreply.github.com>
2025-05-27 18:19:19 +0900

    prefetch

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -5,7 +5,7 @@ const MovieVideos = async ({ id }: { id: string }) => {
     const videos = await getVideos(id);
     return (
         <div className={styles.cont}>
-            {videos.slice(0, 10).map((video) => (
+            {videos.map((video) => (
                 <iframe
                     key={video.id}
                     src={`https://youtube.com/embed/${video.key}`}
